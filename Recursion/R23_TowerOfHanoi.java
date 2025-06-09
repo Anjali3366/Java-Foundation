5245ea354b0a8cf49f3355aaf01e0ed771728eff
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-06-09 15:24:27 +0530

    Add Tower of hanoi & Kth Grammer

--- Recursion/R23_TowerOfHanoi.java
+++ Recursion/R23_TowerOfHanoi.java
@@ -1,3 +1,28 @@
 public class R23_TowerOfHanoi {
+    static int count = 0;
+
+    public static void towerOfHanoi(String source, String helper, String des, int n) {
+        // base case
+        if (n == 1) {
+            count++;
+            System.out.println("Move Plate " + n + " from " + source + " to " + des);
+            return;
+        }
+
+        // hypothesis + induction
+        towerOfHanoi(source, des, helper, n - 1);
+        count++;
+        System.out.println("Move Plate " + n + " from " + source + " to " + des);
+
+        towerOfHanoi(helper, source, des, n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        towerOfHanoi("A", "B", "C", n);
+        System.out.println(count);
+
+    }
 
 }
