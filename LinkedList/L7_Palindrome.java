5c3e972d1999c6aa73992098f27e47b0802e3f04
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-07-16 14:41:07 +0530

    add palindrome in LL

--- LinkedList/L7_Palindrome.java
+++ LinkedList/L7_Palindrome.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class L7_Palindrome {
     public static class Node {
         int data;
@@ -20,8 +23,45 @@ public class L7_Palindrome {
         System.out.println();
     }
 
-    public static void main(String[] args) {
+    public static Node convertToLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node n = new Node(arr[i]);
+            temp.next = n;
+            temp = temp.next;
 
+        }
+        return head;
+
+    }
+
+    public static boolean checkPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+        Node temp = head;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (temp != null) {
+            list.add(temp.data);
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp != null) {
+            if (list.get(list.size() - 1) != temp.data) {
+                return false;
+            }
+            list.remove(list.size() - 1);
+            temp = temp.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 3, 4, 1 };
+        Node head = convertToLL(arr);
+        printLL(head);
+        System.out.println(checkPalindrome(head));
     }
 
 }
